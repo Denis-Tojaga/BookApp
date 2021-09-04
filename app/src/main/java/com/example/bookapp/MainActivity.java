5352886e70fcdd04b14ae2b0a7a7acac512f517d
Denis <denis.tojaga@edu.fit.ba>
2021-09-04 10:14:29 +0200

    Adding new activity AllBooks

--- app/src/main/java/com/example/bookapp/MainActivity.java
+++ app/src/main/java/com/example/bookapp/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.bookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //initializing all UI elements in order to manipulate with their data
+    private Button btnAllBooks, btnAlreadyRead, btnCurrentlyReading, btnYourWishlist, btnFavorite, btnAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //initializing all needed views
+        InitViews();
+    }
+
+    private void InitViews() {
+
+        btnAllBooks = findViewById(R.id.btnAllBooks);
+        btnCurrentlyReading = findViewById(R.id.btnCurrentlyReading);
+        btnAlreadyRead = findViewById(R.id.btnAlreadyRead);
+        btnYourWishlist = findViewById(R.id.btnYourWishlist);
+        btnFavorite = findViewById(R.id.btnSeeFavorites);
+        btnAbout = findViewById(R.id.btnAbout);
+
+    }
+
+    public void btnOnClickOpenAllBooks(View view) {
+        Intent intent = new Intent(MainActivity.this,AllBooksActivity.class);
+        startActivity(intent);
     }
 }
