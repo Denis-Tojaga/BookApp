e3c4f414dcb1366e2e02f2a3b01a23120d9e365d
Denis <denis.tojaga@edu.fit.ba>
2021-09-11 17:02:34 +0200

    Making data persistent

--- app/src/main/java/com/example/bookapp/Utils.java
+++ app/src/main/java/com/example/bookapp/Utils.java
@@ -1,11 +1,21 @@
 package com.example.bookapp;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class Utils {
 
     private static Utils instance;
+    private static final String ALL_BOOKS_KEY = "all_books";
+
+    private SharedPreferences sharedPreferences;
 
     private static ArrayList<Book> allBooks;
     private static ArrayList<Book> alreadyReadBooks;
@@ -15,13 +25,12 @@ public class Utils {
 
 
     //because we will implement singleton pattern we need to set the constructor to private
-    private Utils() {
-        if (allBooks == null) {
-            allBooks = new ArrayList<>();
-            //initializing starting data for the list of books
-            //so whenever we call the Utils constructor we will have some data inside because of this method
+    private Utils(Context context) {
+
+        sharedPreferences = context.getSharedPreferences("alternate_db", Context.MODE_PRIVATE);
+
+        if (getAllBooks() == null)
             InitData();
-        }
 
         if (alreadyReadBooks == null)
             alreadyReadBooks = new ArrayList<>();
@@ -41,6 +50,9 @@ public class Utils {
     private void InitData() {
         //TODO: add initial data
 
+        ArrayList<Book> books = new ArrayList<>();
+
+
         //adding the books to an array list and setting it to adapter
         String longDesc = "The novel is a story of how a woman named Aomame begins to notice strange changes occurring in the world." + "\n" +
                 "She is quickly caught up in a plot involving Sakigake, a religious cult, and her childhood love, Tengo," + "\n" +
@@ -48,25 +60,34 @@ public class Utils {
                 "This is some dummy text." + "\n" + "She is quickly caught up in a plot involving Sakigake"
                 + "\n" + "She is quickly caught up in a plot involving Sakigake";
 
-        allBooks.add(new Book(1, "1Q84", "Haruki Murakami", 1350, "https://s2.adlibris.com/images/2086196/1q84.jpg",
+        books.add(new Book(1, "1Q84", "Haruki Murakami", 1350, "https://s2.adlibris.com/images/2086196/1q84.jpg",
                 "A work of maddening brilliance", longDesc));
 
 
-        allBooks.add(new Book(2, "The myth of Sisyphus", "Albert Camus", 250, "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/820e6a44067829.5806a364b49f8.jpg",
+        books.add(new Book(2, "The myth of Sisyphus", "Albert Camus", 250, "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/820e6a44067829.5806a364b49f8.jpg",
                 "One of the most influential works of this century, this is a crucial exposition of existentialist thought.", "Long description"));
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        editor.putString(ALL_BOOKS_KEY, gson.toJson(books));
+        editor.commit();
     }
 
 
     //getter of the instance
-    public static synchronized Utils getInstance() {
+    public static synchronized Utils getInstance(Context context) {
         if (instance == null)
-            instance = new Utils();
+            instance = new Utils(context);
         return instance;
     }
 
 
     //getters
-    public static ArrayList<Book> getAllBooks() {
+    public ArrayList<Book> getAllBooks() {
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<Book>>() {
+        }.getType();
+        ArrayList<Book> allBooks = gson.fromJson(sharedPreferences.getString(ALL_BOOKS_KEY, null), type);
         return allBooks;
     }
 
@@ -119,16 +140,16 @@ public class Utils {
         return alreadyReadBooks.remove(removedBook);
     }
 
-    public boolean removeFromWantToRead(Book removedBook){
+    public boolean removeFromWantToRead(Book removedBook) {
         return wantToReadBooks.remove(removedBook);
     }
 
 
-    public boolean  removeFromCurrentlyReading(Book removedBook){
+    public boolean removeFromCurrentlyReading(Book removedBook) {
         return currentlyReadingBooks.remove(removedBook);
     }
 
-    public boolean removeFromFavorites(Book removedBook){
+    public boolean removeFromFavorites(Book removedBook) {
         return favoriteBooks.remove(removedBook);
     }
 }
